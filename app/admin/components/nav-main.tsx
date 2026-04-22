15478b01d8422fea6d0fca0764cc08868bc0d3c5
Dwight Entico <dwightentico@gmail.com>
2026-04-22 22:27:37 +0800

    fix: add animation classes to CollapsibleContent for smoother transitions

--- app/admin/components/nav-main.tsx
+++ app/admin/components/nav-main.tsx
@@ -51,7 +51,7 @@ export function NavMain({
                   <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
                 </SidebarMenuButton>
               </CollapsibleTrigger>
-              <CollapsibleContent>
+              <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
                 <SidebarMenuSub>
                   {item.items?.map((subItem) => (
                     <SidebarMenuSubItem key={subItem.title}>
